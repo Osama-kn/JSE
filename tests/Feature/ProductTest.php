441cf9ae49299433de4388f121cceaa353154a54
Osama <oussamaknouz2000@gmail.com>
2023-05-09 21:56:52 +0100

    create product tests

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -6,19 +6,19 @@ use App\Models\Category;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
 
     use DatabaseTransactions, WithFaker;
+
     /**
      * Test Save Product
      *
      * @return void
      */
-    public function testSaveProduct()
+    public function testSaveProduct(): void
     {
         // Create a category to associate with the product
         $category = Category::factory()->create();
@@ -48,5 +48,47 @@ class ProductTest extends TestCase
             'category_id' => $category->id,
             'product_id' => $response->json('data')['id'],
         ]);
+    }
+
+    /**
+     * Tests that a product cannot be saved without any fields.
+     *
+     * @return void
+     */
+    public function testSaveProductWithoutFields(): void
+    {
+        // Make a POST request without any data
+        $response = $this->postJson('/api/products');
+
+        // Assert that the response has a status code of 422 (Unprocessable Entity)
+        $response->assertStatus(422);
+
+        // Assert that the validation errors are returned in the response
+        $response->assertJsonValidationErrors(['name', 'description', 'price', 'image', 'categories_ids']);
+    }
+
+
+    /**
+     * Tests that a product cannot be saved without price and image fields.
+     *
+     * @return void
+     */
+    public function testSaveProductSomeMissingFields(): void
+    {
+        $category = Category::factory()->create();
+
+        // Make a POST request with some fields missing
+        $response = $this->postJson('/api/products', [
+            'name' => 'Test Product',
+            'description' => 'Lorem ipsum dolor sit amet',
+            // The "price" and "image" fields are missing
+            'categories_ids' => [$category->id],
+        ], ['Accept' => 'application/json']);
+
+        // // Assert that the response has a status code of 422
+        $response->assertStatus(422);
+
+        // // Assert that the validation errors are returned in the response
+        $response->assertJsonValidationErrors(['price', 'image']);
     }
 }
